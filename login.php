19e307a691aa841154128a6ec8f7e143084a19d1
LingGubei15 <ling_gubei_15@yahoo.co.jp>
2021-07-10 15:49:34 +0900

    ログインページ、キレイにしました。

--- login.php
+++ login.php
@@ -1,36 +1,52 @@
 <!DOCTYPE html>
-<html lang="ja">
+
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ログイン</title>
+    <title>Document</title>
+
+    <!-- BootstrapのCSS読み込み -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- jQuery読み込み -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- BootstrapのJS読み込み -->
+    <script src="js/bootstrap.min.js"></script>
+
 </head>
 
 <body>
+
     <header>
-        <!-- <h1>サービス名</h1> -->
+        <h1 align=center>ログイン</h1>
     </header>
-    <form action="login_act.php" method="POST">
-        <fieldset>
-            <legend>ログインするには、登録したメールアドレスとパスワードを入力してください</legend>
-            <div>
-                <label for="email">メールアドレス: </label>
-                <input type="email" name="email" id="email">
-            </div>
-            <div>
-                <label for="password">パスワード: </label>
-                <input type="password" name="password" id="password">
-            </div>
-            <div>
-                <button>ログイン</button>
-            </div>
-        </fieldset>
-    </form>
 
-    <form action="register_act.php" method="POST">
+　　ログインするには、メールアドレスとパスワードを入力してください。
+    
+    <div class="input-group">
+        <input type="text" class="form-control" placeholder="メールアドレス" aria-label="" aria-describedby="basic-addon1">
+    </div>
 
-        <fieldset>
+    <div>
+        <input type="text" class="form-control" placeholder="パスワード" aria-label="" aria-describedby="basic-addon1">
+    </div>
+
+    <div class="input-group">
+        <input class="form-control rounded-0" id="exampleFormControlTextarea2" rows="3"></textarea>
+    </div>
+
+    <button type="submit" class="btn btn-primary mb-2">ログインする</button>
+
+    <div>
+
+    </div>
+
+    <footer>
+        <p align=center>フッター</p>
+
+    </footer>
 
 </body>
 
