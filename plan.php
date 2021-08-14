375cee75d9f0ad31afb9725908d0fce381e21399
LingGubei15 <ling_gubei_15@yahoo.co.jp>
2021-08-14 15:27:10 +0900

    イベントを企画するページの開始日時と終了日時をカレンダー式にしました

--- plan.php
+++ plan.php
@@ -18,30 +18,37 @@
     <form action="plan_act.php" method="post">
         <div class="plan_card">
             <div class="plan_card_inner">
-                <div>
+            　　
+            　　　<div>
                     <label for="plan_img">教材イメージ: </label>
                     <input type="file" name="plan_img" id="plan_img">
                 </div>
+                
                 <div>
                     <label for="plan_name">イベント名: </label>
                     <input type="text" name="plan_name" id="plan_name">
                 </div>
+                
                 <div>
                     <label for="plan_start_date">イベント開始日時: </label>
-                    <input type="datetime" name="plan_start_date" id="plan_start_date">
+                    <input type="date" name="birthday">
                 </div>
+               
                 <div>
                     <label for="plan_end_date">イベント終了日時: </label>
-                    <input type="datetime" name="plan_end_date" id="plan_end_date">
+                    <input type="date" name="birthday">
                 </div>
+                
                 <div>
                     <label for="plan_range">学習範囲: </label>
                     <input type="text" name="plan_range" id="plan_range">
                 </div>
+               
                 <div>
                     <label for="plan_limit">人数制限: </label>
                     <input type="number" name="plan_limit" id="plan_limit">
                 </div>
+                
                 <div>
                     <label for="plan_message">主催者から一言: </label>
                     <textarea name="plan_message" id="" cols="30" rows="10"></textarea>
@@ -50,11 +57,11 @@
                 <p>参加メンバー</p>
 
             </div>
+            
             <div>
                 <button>募集する</button>
-
-
             </div>
+            
             <div>
                 <a href="index.php">ホーム画面へ</a>
             </div>
