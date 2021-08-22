20dc5dfc763be4c333d7eefa22a4d1332852f6aa
takuto0419 <tktokada@yahoo.co.jp>
2021-08-22 20:03:07 +0900

    スタッフページ更新

--- index.php
+++ index.php
@@ -136,7 +136,7 @@ if ($status == false) {
             <a href="login.php">ログイン</a>
             <a href="register.html">新規会員登録</a>
             <a href="profile_edit.php">会員情報変更</a>
-            <a href="staff_page.html">スタッフ一覧</a>
+            <a href="staff_page.php">スタッフ一覧</a>
             <a href="qanda.html">お問い合わせ</a>
 
             </div>
--- staff_page.php
+++ staff_page.php
@@ -48,6 +48,20 @@
         </div>
       </div>
 
+      <div class="card text-white bg-danger mb-3 rounded-pill" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="img/staff/Iwasa_icon.png" width="180" height="180" alt="画像">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">イワサ</h5>
+              <p class="card-text">絵を描く事と、読書と、猫が大好きです。<br>プログラミング（HTMLとphp）を勉強していて、<br>今は、この教室のホームページづくりに参加しています。</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div class="card text-white bg-danger mb-3" style="max-width: 540px;">
         <div class="row g-0">
           <div class="col-md-4">
