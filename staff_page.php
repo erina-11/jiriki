3cc91f02d67df9970e9e52198b982edfc6702f14
takuto0419 <tktokada@yahoo.co.jp>
2021-09-18 14:27:44 +0900

    こっそりアップデート

--- staff_page.php
+++ staff_page.php
@@ -42,7 +42,7 @@
           <div class="col-md-8">
             <div class="card-body">
               <h5 class="card-title">オカダ</h5>
-              <p class="card-text">プログラミングを勉強中の受験生。<br>このページ作ってます。<br>「いつか自分でゲーム作りたいなぁ」<br>と漠然と考えてますが、一体いつになるやら...</p>
+              <p class="card-text">プログラミングを勉強中の受験生。<br>このページとか作ってます。<br>「いつか自分でゲーム作りたいなぁ」<br>と漠然と考えてますが、一体いつになるやら...</p>
             </div>
           </div>
         </div>
@@ -62,6 +62,20 @@
         </div>
       </div>
 
+      <div class="card text-white bg-danger mb-3 rounded-pill" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="img/staff/Hi_icon.png" width="180" height="180" alt="画像">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Hi****</h5>
+              <p class="card-text">いま、HTML、CSSの勉強をしています。<br>いつか、自分でアプリを作ってみたいです。</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div class="card text-white bg-danger mb-3" style="max-width: 540px;">
         <div class="row g-0">
           <div class="col-md-4">
@@ -76,6 +90,8 @@
         </div>
       </div>
     </main>
+
+    <?php include('footer.php'); ?>
 
 </body>
 </html>
