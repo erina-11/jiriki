477b31173d0fc5b579dadbdca729a04b191a01aa
044073099 <hisaki714@yahoo.co.jp>
2021-11-13 20:57:09 +0800

    ok

--- plan.php
+++ plan.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
     <title>イベントを企画するページ</title>
 </head>
 
@@ -19,51 +20,54 @@
     <form action="plan_act.php" method="post">
         <div class="plan_card">
             <div class="plan_card_inner">
-            　　
-            　　　<div>
-                    <label for="plan_img">教材イメージ: </label>
-                    <input type="file" name="plan_img" id="plan_img">
-                </div>
-                
-                <div>
-                    <label for="plan_name">イベント名: </label>
-                    <input type="text" name="plan_name" id="plan_name">
-                </div>
-                
-                <div>
-                    <label for="plan_start_date">イベント開始日時: </label>
-                    <input type="date" name="plan_start_date">
-                </div>
-               
-                <div>
-                    <label for="plan_end_date">イベント終了日時: </label>
-                    <input type="date" name="plan_end_date">
-                </div>
-                
-                <div>
-                    <label for="plan_range">学習範囲: </label>
-                    <input type="text" name="plan_range" id="plan_range">
-                </div>
-               
-                <div>
-                    <label for="plan_limit">人数制限: </label>
-                    <input type="number" min="1" name="plan_limit" id="plan_limit">
-                </div>
-                
-                <div>
-                    <label for="plan_message">主催者から一言: </label>
-                    <textarea name="plan_message" id="" cols="30" rows="10"></textarea>
-                </div>
 
-                <p>参加メンバー</p>
+            <table class="table">
+  <thead>
+    <tr>
+      <th></th>
+      <th></th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row"></th>
+      <td><label for="plan_img">教材イメージ: </label></td>
+      <td><input type="file" name="plan_img" id="plan_img"></td>
+    </tr>
+    <tr>
+      <th scope="row"2</th>
+      <td><label for="plan_name">イベント名: </label></td>
+      <td><input type="text" name="plan_name" id="plan_name"></td>
+    </tr>
+    <tr>
+    <th scope="row"3</th>
+     <td> <label for="plan_start_date">イベント開始日時: 
+     <td><input type="date" name="plan_start_date"></td>
+     </tr>
+     <tr>
+<th scope="row"2</th>
+ <td><label for="plan_end_date">イベント終了日時: </label></td>
+ <td><input type="date" name="plan_end_date"></td>
+ </tr>
+ <tr>
+<th scope="row"2</th>
+ <td><label for="plan_range">学習範囲: </label></td>
+ <td><input type="text" name="plan_range" id="plan_range"></td>
+ </tr>
+ <tr>
+<th scope="row"2</th>
+ <td><label for="plan_limit">人数制限: </label> </label></td>
+ <td><input type="number" min="1" name="plan_limit" id="plan_limit"></td>
+ </tr>
+ <tr>
+<th scope="row"2</th>
+ <td><label for="plan_message">主催者から一言: </label></td>
+ <td><textarea name="plan_message" id="" cols="30" rows="10"></textarea>
+ <button>募集する</button></td>
+ </tr>
+ <tr>
 
-            </div>
-            
-            <div>
-　　　　　　 <button>募集する</button>
-            </div>
-            
-            <div>
                 <a href="index.php">ホーム画面へ</a>
             </div>
 
