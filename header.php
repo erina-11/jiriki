d9d71acf5171e16959718b14802c67d501ff536c
nezumiyaro100 <nezumiyaro100@gmail.com>
2022-03-05 12:05:51 +0800

    ヘッダーが出てなかったので修正

--- header.php
+++ header.php
@@ -1,8 +1,8 @@
 <?php
-if (empty($_SESSION['id'])){
-    $user_id = "" ;
+if (empty($_SESSION['id'])) {
+    $user_id = "";
 } else {
-    $user_id = $_SESSION ;
+    $user_id = $_SESSION;
 }
 ?>
 
@@ -26,21 +26,19 @@ if (empty($_SESSION['id'])){
 
 <body>
     <header>
-        <div class = "img">
+        <div class="img">
             <a href="index.php"> <img src="img/logo.png" width="252" height="54" alt=""> </a>
         </div>
-        <nav id="top-nav">
-            <ul>
-                <li> <a href="about.php">ABOUT</a> </li>
-                <li> <a href="index.html#menu">MENU</a> </li>
-                <li>
-                    <?php if ($user_id) {  ?>
+        <ul>
+            <li> <a href="about.php">ABOUT</a> </li>
+            <li> <a href="index.html#menu">MENU</a> </li>
+            <li>
+                <?php if ($user_id) {  ?>
                     <a href="mypage.php">MYPAGE</a>
                     <a href="logout.php">LOGOUT</a>
-                    <?php } else { ?>
+                <?php } else { ?>
                     <a href="login.php">LOGIN</a>
                     <a href="register.html">SIGNIN</a>
-                    <?php } ?>
-                </li>
-            </ul>
-        </nav>
+                <?php } ?>
+            </li>
+        </ul>
