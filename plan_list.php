92010c8f16b5c7c73c18c21ca6f341a923bc1eb9
LingGubei15 <ling_gubei_15@yahoo.co.jp>
2022-09-24 15:47:28 +0900

    とちゅうですが…

--- plan_list.php
+++ plan_list.php
@@ -4,7 +4,7 @@ session_start();
 include('functions.php');
 
 if (!empty($_SESSION['id'])) {
-    $user_id = $_SESSION['id'];
+  $user_id = $_SESSION['id'];
 }
 
 // var_dump($user_id);
@@ -22,52 +22,39 @@ $status = $stmt->execute();
 
 // データ登録処理後
 if ($status == false) {
-    // SQL実行に失敗した場合はここでエラーを出力し，以降の処理を中止する
-    $error = $stmt->errorInfo();
-    echo json_encode(["error_msg" => "{$error[2]}"]);
-    exit();
+  // SQL実行に失敗した場合はここでエラーを出力し，以降の処理を中止する
+  $error = $stmt->errorInfo();
+  echo json_encode(["error_msg" => "{$error[2]}"]);
+  exit();
 } else {
-    // 正常にSQLが実行された場合は入力ページファイルに移動し，入力ページの処理を実行する
-    // fetchAll()関数でSQLで取得したレコードを配列で取得できる
-    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);  // データの出力用変数（初期値は空文字）を設定
-    $output = "";
+  // 正常にSQLが実行された場合は入力ページファイルに移動し，入力ページの処理を実行する
+  // fetchAll()関数でSQLで取得したレコードを配列で取得できる
+  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);  // データの出力用変数（初期値は空文字）を設定
+  $output = "";
 
-    // <tr><td>deadline</td><td>todo</td><tr>の形になるようにforeachで順番に$outputへデータを追加
-    // `.=`は後ろに文字列を追加する，の意味
+  // <tr><td>deadline</td><td>todo</td><tr>の形になるようにforeachで順番に$outputへデータを追加
+  // `.=`は後ろに文字列を追加する，の意味
 }
 
 $output .= "<div class='d-flex flex-wrap'>";
 
 foreach ($result as $record) {
 
-    $output .= "
-      <a href='plan_details.php?id={$record["id"]}'>
-        <div class='action_expansion'>
-          <div class='card mb-3 bg-info text-white' style='max-width: 540px;'> 
-            <div class='row g-0'>
-              <div class='col-md-4'>
-                <img src='img/{$record["img"]}' alt='ここには写真が出ます' width='150' height='100'>
-              </div>
-              <div class='col-md-8'>
-                <div class='card-body'>
-                  <h5 class='card-title'>{$record["name"]}</h5>
-                  <p class='card-text'>{$record["organizer_message"]}'</p>
-                </div>
-              </div>
-            </div>
-            <div class='row g-0'>
-                <div class='card-body'>
-              <p class='card-text text-white'>
-                主催者からのひとこと
-              </p>
-                </div>
-            </div>
-          </div>
-        </div>
-     </a>
-    ";
+  $output .= "
+  <a href='plan_details.php?plan_id={$record["id"]}'>
+  <div class='ueno'> 
+      <img src='img/{$record["img"]}' alt='ここには写真が出ます' width='150' height='100'>
+      <h5 class='addition2'>{$record["name"]}</h5>
+      <p class='card-text'>{$record["organizer_message"]}'</p>
+      <p>
+      
+      <a class='addition1'> 主催者からのひとこと 
+      </p>
+  </div>
+</a>
+";
 
-    /*
+  /*
         $output .= "<tr>";
         $output .= "<td><a href=''plan_details.php?id={$record["id"]}''plan_details.php?id={$record["id"]}''plan_details.php?id={$record["id"]}''plan_details.php?id={$record["id"]}''plan_details.php?id={$record["id"]}''>{$record["name"]}</a></td>";
         $output .= "<td>" . date("Y/n/j", strtotime($record["date_start"])) . "</td>";
@@ -80,28 +67,29 @@ foreach ($result as $record) {
         // $output .= "<td><a href='todo_delete.php?id={$record["id"]}'>delete</a></td>";
         $output .= "</tr>";
 */
-    // $valueの参照を解除する．解除しないと，再度foreachした場合に最初からループしない
-    // 今回は以降foreachしないので影響なし
-    unset($value);
+  // $valueの参照を解除する．解除しないと，再度foreachした場合に最初からループしない
+  // 今回は以降foreachしないので影響なし
+  unset($value);
 }
 $output .= "</div>";
 
 ?>
 
 
+<?php include('head_top.php'); ?>
 
-<?php include('header.php'); ?>
-</header>
+<body>
+  <?php include('header_common.php'); ?></header>
 
-<!-- <button>ホーム</button> -->
+  <!-- <button>ホーム</button> -->
 
-</div>
-</div>
+  </div>
+  </div>
 
-<!-- ここに<tr><td>deadline</td><td>todo</td><tr>の形でデータが入る -->
-<?= $output ?>
+  <!-- ここに<tr><td>deadline</td><td>todo</td><tr>の形でデータが入る -->
+  <?= $output ?>
 
-<?php include('footer.php'); ?>
+  <?php include('footer.php'); ?>
 
 
 </body>
